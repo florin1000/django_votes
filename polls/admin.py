653c132688c72883758acab1eadfff2bfe903aaf
Florin <oancea.quentin.florin@gmail.com>
2019-06-24 18:43:10 +0300

    dynamic url, detail.html and index.html views created

--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Question
+
+# set the Question table to be accessed thru the admin
+admin.site.register(Question)
