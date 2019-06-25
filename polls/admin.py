92f878a0e39d4fb66d7ba1e6b18ed923d29840cc
Florin <oancea.quentin.florin@gmail.com>
2019-06-25 10:34:03 +0300

    create results view

--- polls/admin.py
+++ polls/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import Question
+from .models import Question, Choice
 
 # set the Question table to be accessed thru the admin
 admin.site.register(Question)
+admin.site.register(Choice)
